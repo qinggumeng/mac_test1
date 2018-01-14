a6e94d3e9fbcab18ce2c1d974f9e17b6ae74f41f
qinggumeng <353782686@qq.com>
2018-01-14 13:34:43 +0800

    ssssss

---
+++ web1/src/com/lxd/entity/Person.java
@@ -0,0 +1,13 @@
+package com.lxd.entity;
+
+public class Person {
+    private String name1;
+
+    public String getName() {
+        return name1;
+    }
+
+    public void setName(String name) {
+        this.name1 = name;
+    }
+}
